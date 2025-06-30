e24cb90ffb1a85c14d444a36bab2394366d4be40
HellenCristina <hellencrissaraujo28@gmail.com>
2025-06-30 14:13:47 -0300

    Atualizando meu design

--- src/App.jsx
+++ src/App.jsx
@@ -26,7 +26,7 @@ function App() {
   }, []);
   return (
     <div class="mx-auto">
-      <nav class="fixed top-0 w-full bg-purple-800 px-15 py-2 border-b-2 border-amber-400 shadow-md sm:hidden lg:px-50" id='nav'>
+      <nav class="fixed top-0 w-full bg-purple-800 px-15 py-2 border-b-2 border-amber-400 shadow-md sm:hidden lg:px-40" id='nav'>
         <ul class="flex justify-between text-[#f1f1f1]/50 font-semibold">
           <li><a href="#home" class="hover:text-[#f1f1f1]">Home</a></li>
           <li><a href="#habilidades" class="hover:text-[#f1f1f1]">Habilidades</a></li>
@@ -34,7 +34,7 @@ function App() {
           <li><a href="#redessociais" class="hover:text-[#f1f1f1]">Redes Sociais</a></li>
         </ul>
       </nav>
-      <header class="bg-gradient-to-t from to-65% to-purple-800 px-8 pt-15 lg:px-50" id='home'>
+      <header class="bg-gradient-to-t from to-65% to-purple-800 px-8 pt-15 lg:px-40" id='home'>
         <img src="" alt="Foto minha" class="block float-right rounded-full h-20 sm:h-30 lg:h-50 lg:mx-3" id='imgG'/>
         <h1 class="text-4xl font-extrabold sm:text-5xl lg:text-6xl">{name}</h1>
         <h2>{objective}</h2>
@@ -48,7 +48,7 @@ function App() {
         <p>Estou cursando {subjects}, na {colegy} no 3º semestre da faculdade.</p>
         <br />
       </header>
-      <main class="px-8 pt-15 lg:px-50">
+      <main class="px-8 pt-15 lg:px-40">
         <h2 id='habilidades'>Habilidades</h2>
         <ul class="grid grid-cols-1 gap-2 sm:grid-cols-2">
           <li>HTML
@@ -94,7 +94,7 @@ function App() {
         <a href="./curriculo/Curriculo_Hellen_Estágio_FrontEnd.pdf" download="./curriculo/Curriculo_Hellen_Estágio_FrontEnd.pdf" class="cursor-pointer py-1 px-4 mx-2 bg-purple-800 text-[#f1f1f1]/50 rounded-3xl hover:text-[#f1f1f1] hover:bg-purple-800/70 transition font-semibold shadow-md sm:py-2.5">Baixar curriculo</a>
 
         <h2 class="py-5" id='projetos'>Projetos</h2>
-        <article class="grid grid-cols-2 gap-3 px-8 pt-3 lg:px-50">
+        <article class="grid grid-cols-1 gap-3 px-5 pt-2 md:grid-cols-2">
           <Projetos nameproject={"SlideShow em JS"} link={"https://hellencristinap.github.io/slidesShow/"} linkRepo={"https://github.com/HellenCristinaP/slidesShow.github.io"} icon={"fa-solid fa-images"} />
 
           <Projetos nameproject={"Impar ou par"} link={"https://hellencristinap.github.io/numeros/"} linkRepo={"https://github.com/HellenCristinaP/numeros"}
