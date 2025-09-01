9908adcdf91cf50e49af7f12618a6ecaa925eeba
HellenCristina <hellencrissaraujo28@gmail.com>
2025-09-01 11:21:02 -0300

    adicionando e melhorando a acessibilidade com aria

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { useEffect } from 'react';
 import Block from './block'
 import BlockC from './blockC'
 import Projetos from './projetos'
+import Propriedades from './prioridades.jsx'
 
 function App() {
   // Dados do portifólio
@@ -34,24 +35,22 @@ function App() {
     <div className="mx-auto">
       <div className="fixed md:static">
         <div className="fixed right-0 m-3 md:hidden">
-          <label>
-            <div
+          <button type="checkbox" onClick={abrirMenu}>
+            <span
               className="w-9 h-10 cursor-pointer flex flex-col items-center justify-center"
             >
-            <input className="hidden peer" type="checkbox" onClick={abrirMenu}/>
-              <div
+              <span
                 className="w-[80%] h-[2px] bg-white rounded-sm transition-all duration-300 origin-left translate-y-[0.75rem] peer-checked:rotate-[-45deg]"
-              ></div>
-              <div
+              ></span>
+              <span
                 className="w-[60%] h-[2px] bg-white rounded-md transition-all duration-300 origin-center peer-checked:hidden"
-              ></div>
-              <div
+              ></span>
+              <span
                 className="w-[80%] h-[2px] bg-white rounded-md transition-all duration-300 origin-left -translate-y-[0.75rem] peer-checked:rotate-[45deg]"
-                ></div>
-            </div>
-          </label>
-          </div>
-
+              ></span>
+            </span>
+          </button>
+        </div>
         <nav className="bg-purple-900 border-b-amber-400 size-svh p-7 hidden md:block md:justify-items-center md:border-b-2 border-amber-400 md:p-2 md:w-full md:h-fit md:bg-purple-800" id='nav'>
           <ul className="flex flex-col capitalize justify-between text-[#f1f1f1]/50 font-semibold text-2xl mx-2 gap-20
         md:flex-row md:size-fit md:gap-15">
@@ -63,18 +62,19 @@ function App() {
         </nav>
       </div>
       <header className="bg-gradient-to-t to-65% to-purple-800 px-8 pt-25.5 lg:px-40" id='home'>
-        <img src="" alt="Foto minha" className="block float-right rounded-full h-25 sm:h-30 lg:h-45 lg:mx-3" id='imgG' />
+        <img src="https://avatars.githubusercontent.com/u/76890061?v=4" alt="Foto minha | uma garota branca que usa óculos e tem com cabelos longos e pretos" className="block float-right rounded-full h-25 sm:h-30 lg:h-45 lg:mx-3" id='imgG' />
         <h1 className="text-4xl font-extrabold sm:text-5xl lg:text-6xl">{name}</h1>
-        <h2>{objective}</h2>
-        <br />
+        <h2 className="mt-1 mb-5 font-semibold">{objective}</h2>
         <p>Olá, meu nome é {name}, sou {iam} e tenho desejo ser {objective}. <br />
           Trabalhei por mais de 1 ano com atendimento ao cliente e agora quero migrar a programação, amo a sensação de códigos virando uma página web. Tenho afinidade em front-end, mas pretendo ser Full Stack, então estou aberta a aprender.</p>
-        <main className="m-2 p-5 border-2 border-white rounded-2xl">
-          <h2>Formação Acadêmica</h2>
-          <h3 className="text-2xl lg:text-3xl">{subjects}</h3>
-          <p>Estou cursando {subjects}, na {colegy} - {semestre} semestre</p>
+        <main>
+          <section className="mx-3 my-5 p-5 border-2 border-white rounded-2xl">
+            <h2>Formação Acadêmica</h2>
+            <h3 className="text-2xl lg:text-3xl">{subjects}</h3>
+            <p>Estou cursando {subjects}, na {colegy} - {semestre} semestre</p>
+          </section>
+          {/* <Propriedades /> */}
         </main>
-        <br />
       </header>
       <main className="px-8 pt-15 lg:px-40">
         <h2 id='habilidades'>Habilidades</h2>
---
+++ src/prioridades.jsx
@@ -0,0 +1,22 @@
+function prioridades() {
+  return (
+    <div>
+        <h1 className="text-2xl">O que eu prezo</h1>
+        <section className="border rounded-md p-4 m-2 flex justify-center">
+            <h2 className="">Responsividade</h2>
+            <p></p>
+        </section>
+        <section className="border rounded-md p-4 m-2 flex justify-center">
+            <h2>Acessibilidade</h2>
+            <p></p>
+        </section>
+        <section className="border rounded-md p-4 m-2 flex justify-center">
+            <h2>Organização</h2>
+            <p></p>
+        </section>
+
+    </div>
+  );
+}
+
+export default prioridades
